d8b1b13d28d731bdd3d594c4cf78a4e4179dca80
David Steinich <drsteini@usgs.gov>
2015-04-22 08:14:29 -0500

    Adjust for new directory locations and bug fixes in station

--- wqx_dump.sh
+++ wqx_dump.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 DATE_SUFFIX=`date +%Y%m%d_%H%M`
 HTTP_BASE=http://www.epa.gov/storet/download/storetw
-WORK_DIR=/pdc/wqp_data
+
+# display usage message
+function usage() {
+	cat <<EndUsageText
+
+Usage: `basename $0` DB_FOLDER
+
+	This script pulls wqx data exports from the EPA.
+		
+DB_FOLDER
+	One of these must be specified. If more than one is set, the last one parsed will win.
+			
+	cinolog   download to the ci environment
+	devnolog  download to the dev environment
+	qanolog   download to the qa environment
+	dbnolog   download to the production environment
+		
+EndUsageText
+}
 
 # output of this script is parsed and looks for this text to raise errors
 function stop_bad() {
@@ -18,6 +36,32 @@ function stop_ok() {
 # set so if any command in a piped sequence returns a non-zero error code, the script fails
 set -o pipefail
 
+# if not exactly one parameter, display usage and quit
+[ "$#" -ne 1 ] && usage && stop_bad
+
+# parse arguments
+for arg in "$@"
+do
+	case $arg in
+		cinolog)
+			DB_FOLDER=$arg
+			;;
+		devnolog)
+			DB_FOLDER=$arg
+			;;
+		qanolog)
+			DB_FOLDER=$arg
+			;;
+		dbnolog)
+			DB_FOLDER=$arg
+			;;
+	esac
+done
+
+# if any required variables are null or empty, display usage and quit
+[ ! -n "${EXPORT_TYPE}" ] && usage && stop_bad
+
+WORK_DIR=/mnt/wqp_data/${DB_FOLDER}
 EXPORT_REF="owpub_vmwaters1_wqx_Weekly_expdp.ref"
 EXPORT_LOG="owpub_vmwaters1_wqx_Weekly_expdp.log"
 DUMP_FILE_GREP="owpub_vmwaters1_wqx_Weekly_...cdmp"
