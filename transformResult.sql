79c083a6f4570b671aca9477fbbc715c53d5ed94
David Steinich <drsteini@usgs.gov>
2015-06-12 09:21:35 -0500

    truncate/insert rather than drop create wqx_activity

--- transformResult.sql
+++ transformResult.sql
@@ -87,9 +87,26 @@ select /*+ parallel(4) */ res_uid, rdqlmt_measure, msunt_cd, dqltyp_name
 commit;
 
 
-create table wqx_activity as
+prompt populating wqx_activity
+truncate table wqx_activity;
+insert /*+ append parallel(4) */
+  into wqx_activity (act_uid, station_id, site_id, event_date, activity, sample_media, organization, site_type, huc, governmental_unit_code,
+                     organization_name, activity_type_code, activity_media_subdiv_name, activity_start_time, act_start_time_zone,
+                     activity_stop_date, activity_stop_time, act_stop_time_zone, activity_relative_depth_name, activity_depth,
+                     activity_depth_unit, activity_depth_ref_point, activity_upper_depth, activity_upper_depth_unit, activity_lower_depth,
+                     activity_lower_depth_unit, project_id, activity_conducting_org, activity_comment, activity_latitude, activity_longitude,
+                     activity_source_map_scale, act_horizontal_accuracy, act_horizontal_accuracy_unit, act_horizontal_collect_method,
+                     act_horizontal_datum_name, assemblage_sampled_name, act_collection_duration, act_collection_duration_unit,
+                     act_sam_compnt_name, act_sam_compnt_place_in_series, act_reach_length, act_reach_length_unit, act_reach_width,
+                     act_reach_width_unit, act_pass_count, net_type_name, act_net_surface_area, act_net_surface_area_unit, act_net_mesh_size,
+                     act_net_mesh_size_unit, act_boat_speed, act_boat_speed_unit, act_current_speed, act_current_speed_unit,
+                     toxicity_test_type_name, sample_collect_method_id, sample_collect_method_ctx, sample_collect_method_name,
+                     act_sam_collect_meth_qual_type, act_sam_collect_meth_desc, sample_collect_equip_name, act_sam_collect_equip_comments,
+                     act_sam_prep_meth_id, act_sam_prep_meth_context, act_sam_prep_meth_name, act_sam_prep_meth_qual_type,
+                     act_sam_prep_meth_desc, sample_container_type, sample_container_color, act_sam_chemical_preservative,
+                     thermal_preservative_name, act_sam_transport_storage_desc)
 select /*+ parallel(4) */ 
-activity.act_uid,
+       activity.act_uid,
                activity.mloc_uid station_id, 
                station.site_id,
                trunc(activity.act_start_date) event_date,
