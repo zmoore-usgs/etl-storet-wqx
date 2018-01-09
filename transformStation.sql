cbe586fe302db500612aa07b66701ce38fac4976
Stanley Kaymen <skaymen@usgs.gov>
2018-01-09 15:18:15 -0600

    Use county_geom instead of lookup

--- transformStation.sql
+++ transformStation.sql
@@ -153,16 +153,16 @@ merge into wqx_station_local o
       using (select /*+ parallel(4) */ 
                     station_source,
                     station_id,
-                    statefp,
-                    countyfp
-               from county_geom_lookup,
+                    fips_state_code,
+                    fips_county_code
+               from county_geom,
                     wqx_station_local
-              where sdo_contains(county_geom_lookup.geom,  wqx_station_local.geom) = 'TRUE' and
+              where sdo_contains(county_geom.geom,  wqx_station_local.geom) = 'TRUE' and
                     cntry_cd in ('AS','PR','UM','US', 'VI') and
                     calculated_fips is null) n
    on (o.station_id = n.station_id and
        o.station_source = n.station_source)
-when matched then update set calculated_fips = statefp || countyfp;
+when matched then update set calculated_fips = fips_state_code || fips_county_code;
 commit;
 select 'Calculating Geopolitical Data complete: ' || systimestamp from dual;
 
