d7097c7e724e2a2cf800cb56d57746d156d1e1a6
Stanley Kaymen <skaymen@usgs.gov>
2018-01-12 15:36:58 -0600

    fix additional typo

--- transformStation.sql
+++ transformStation.sql
@@ -84,7 +84,7 @@ delete from wqx_station_local
  where station_source = 'STORETW' and
        not exists (select null
                      from station_no_source
-                    where wqx_station_local.station_id = station_no_source.station_id;
+                    where wqx_station_local.station_id = station_no_source.station_id);
 commit;
 select 'Delete missing no_source from wqx_station_local complete: ' || systimestamp from dual;
 
